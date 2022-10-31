d46263aee78dc03284a90818f42da2bb35298b5c
Rarka1-01 <artoym.kalinichenko@yandex.ru>
2022-10-31 13:36:02 +0700

    <Required>

--- ELTEX2_4/main.cpp
+++ ELTEX2_4/main.cpp
@@ -1,11 +1,11 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include "btree.h"
-
+//<Required>
 int main()
 {
 	system("chcp 1251");
 	system("cls");
-	//srand(time(NULL));
+	//srand(time(NULL)); 
 	node* root = NULL;
 	
 	push(&root, rand() % 10);
